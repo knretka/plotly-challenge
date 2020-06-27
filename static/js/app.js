45b9ea53d1e57f82640d3d79242aad52f2fb08f3
knretka <kaylene.retka@gmail.com>
2020-06-27 16:03:52 -0500

    updating app.js

--- static/js/app.js
+++ static/js/app.js
@@ -60,14 +60,14 @@ function buildPlot(sample){
         Plotly.newPlot('bubble', data1, layout);
 
  //Bar Chart       
-        var y_lab = otu_ids
+        var y_lab = otu_ids.splice(0, 10).reverse()
         y_lab = y_lab.map(i => 'OTU ' + i);
         
         var trace2 = {
           type: 'bar',
-          x: sample_values,
+          x: sample_values.splice(0,10).reverse(),
           y: y_lab,
-          text: otu_labels,
+          text: otu_labels.splice(0,10).reverse(),
           orientation: 'h'
           };
 
